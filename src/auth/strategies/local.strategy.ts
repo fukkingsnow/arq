68ff9fd58913147f9f2c5b92922fcf04876e69dc
fukkingsnow <fukkingsnow@gmail.com>
2025-12-07 20:55:29 +0300

    fix/Phase 26: Fix User entity import in local.strategy - Correct path to entities (103 lines)

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../services/auth.service';
-import { User } from '../../users/entities/user.entity';
+import { User } from '../../entities/user.entity';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
