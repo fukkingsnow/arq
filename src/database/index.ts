c7ef8a2fe1717efaaa7c017ddfc754655cffce3e
fukkingsnow <fukkingsnow@gmail.com>
2025-12-07 12:22:29 +0300

    feat/Phase 26: Add Database Module barrel export - index.ts (7 lines)

---
+++ src/database/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Database Module - Barrel Export
+ * Exports TypeORM configuration module for database connection
+ */
+
+export { TypeOrmDatabaseModule } from './typeorm.module';
+export { TypeOrmDatabaseModule as DatabaseModule } from './typeorm.module';
