40d5dd90973dada9f704a3343e5ecd10496bb80c
fukkingsnow <fukkingsnow@gmail.com>
2025-12-25 00:19:35 +0300

    Create index.ts
    M1.2 Task Queue Infrastructure - Add barrel exports for queue module with public API.

---
+++ src/queue/index.ts
@@ -0,0 +1,3 @@
+export { QueueModule } from './queue.module';
+export { RedisQueueService, QueueConfig, QueueJobData } from './redis-queue.service';
+export { TaskConsumerService, TaskPayload } from './task-consumer.service';
