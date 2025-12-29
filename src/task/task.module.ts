d20d1ec68e4028d3555bd9a5a94db83d54426ce7
fukkingsnow <fukkingsnow@gmail.com>
2025-12-29 16:26:05 +0300

    Update task.module.ts

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DevelopmentTaskEntity } from '../entities/development-task.entity';
-import { TaskRepository } from './repositories/task.repository';
 import { TaskService } from './services/task.service';
 import { TaskController } from './controllers/task.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DevelopmentTaskEntity])],
   controllers: [TaskController],
-  providers: [TaskService, TaskRepository],
+  providers: [TaskService],
   exports: [TaskService],
 })
 export class TaskModule {}
