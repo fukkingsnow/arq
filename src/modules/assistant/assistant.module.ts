6895d0ed9c3758ed8436506fee0fb894f200251d
fukkingsnow <fukkingsnow@gmail.com>
2026-01-12 16:16:48 +0300

    fix: Remove problematic service imports from assistant module

--- src/modules/assistant/assistant.module.ts
+++ src/modules/assistant/assistant.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AssistantController } from './assistant.controller';
-import { QueryAnalyzerService } from './services/query-analyzer.service';
-import { StatusQueryService } from './services/status-query.service';
-import { ErrorDetectionService } from './services/error-detection.service';
 
 @Module({
   controllers: [AssistantController],
-  providers: [
-    QueryAnalyzerService,
-    StatusQueryService,
-    ErrorDetectionService,
-  ],
-  exports: [
-    QueryAnalyzerService,
-    StatusQueryService,
-    ErrorDetectionService,
-  ],
+  providers: [],
+  exports: [],
 })
 export class AssistantModule {}
