5b698b7ea5fccabd2958d494db80e6a009fd7709
fukkingsnow <fukkingsnow@gmail.com>
2026-01-14 11:01:11 +0300

    fix: Fix TasksService typing and async methods

--- src/modules/tasks.service.ts
+++ src/modules/tasks.service.ts
@@ -2,33 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from '../entities/task.entity';
-import { CreateTaskDto } from '../dto/create-task.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(Task)
-    private tasksRepository: Repository<Task>,
+    private readonly taskRepository: Repository<Task>,
   ) {}
 
   async create(createTaskDto: CreateTaskDto): Promise<Task> {
-const task = this.taskRepository.create({ ...createTaskDto, status: 'pending' });
-    return this.taskRepository.save(task)  }
+    const task = this.taskRepository.create({
+      ...createTaskDto,
+      status: 'pending',
+    });
+    return this.taskRepository.save(task);
+  }
 
   async findAll(): Promise<Task[]> {
-    return this.tasksRepository.find();
+    return this.taskRepository.find();
   }
 
   async findOne(id: string): Promise<Task> {
-    return this.tasksRepository.findOne({ where: { id } });
+    return this.taskRepository.findOne({ where: { id } });
   }
 
   async update(id: string, createTaskDto: CreateTaskDto): Promise<Task> {
-    await this.tasksRepository.update(id, createTaskDto);
+    await this.taskRepository.update(id, createTaskDto);
     return this.findOne(id);
   }
 
   async remove(id: string): Promise<void> {
-    await this.tasksRepository.delete(id);
+    await this.taskRepository.delete(id);
   }
 }
