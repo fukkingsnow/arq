96c87ed13ba76d225d4797865f160da22905aa63
fukkingsnow <fukkingsnow@gmail.com>
2026-01-20 10:20:53 +0300

    Add TaskRepository to tasks module providers

--- src/modules/tasks.module.ts
+++ src/modules/tasks.module.ts
@@ -4,8 +4,9 @@ import { Task } from '../entities/task.entity';
 import { TasksService } from './tasks.service';
 import { TaskExecutorService } from './task-executor.service';
 import { TaskController } from '../controllers/task.controller';
+import { TaskRepository } from '../repositories/task-repository';
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
-providers: [TasksService, TaskExecutorService],  controllers: [TaskController],
+providers: [TasksService, TaskExecutorService], TaskRepository,  controllers: [TaskController],
 })
 export class TasksModule {}
