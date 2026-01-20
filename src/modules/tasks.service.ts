4ccb2ca4e8b7416cd3247433be18c48b0a8fe6ea
fukkingsnow <fukkingsnow@gmail.com>
2026-01-20 10:19:22 +0300

    Refactor: Use TaskRepository instead of TypeORM Repository directly

--- src/modules/tasks.service.ts
+++ src/modules/tasks.service.ts
@@ -1,35 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { TaskRepository } from '../repositories/task-repository';
 import { Task } from '../entities/task.entity';
 
 @Injectable()
 export class TasksService {
   constructor(
-    @InjectRepository(Task)
-    private readonly taskRepository: Repository<Task>,
+    private readonly taskRepository: TaskRepository,
   ) {}
 
   async create(data: any): Promise<Task> {
-    const task = this.taskRepository.create({
+    return this.taskRepository.create({
       ...data,
       status: 'pending',
     });
-    const savedTask = await this.taskRepository.save(task);
-    return Array.isArray(savedTask) ? savedTask[0] : savedTask;
   }
 
   async findAll(): Promise<Task[]> {
-    return this.taskRepository.find();
+    return this.taskRepository.findAll();
   }
 
   async findOne(id: string): Promise<Task | null> {
-    return this.taskRepository.findOne({ where: { id } });
+    return this.taskRepository.findById(id);
   }
 
   async update(id: string, data: any): Promise<Task | null> {
-    await this.taskRepository.update(id, data);
-    return this.findOne(id);
+    return this.taskRepository.update(id, data);
   }
 
   async remove(id: string): Promise<void> {
