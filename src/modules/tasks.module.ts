5de59d949cec39ed539eafce34f73ec23611c721
fukkingsnow <fukkingsnow@gmail.com>
2026-01-26 16:42:13 +0300

    fix(phase40): Fix tasks.module.ts syntax error  Removed non-existent TaskExecutorService import and fixed module configuration

--- src/modules/tasks.module.ts
+++ src/modules/tasks.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from '../entities/task.entity';
 import { TasksService } from './tasks.service';
-import { TaskExecutorService } from './task-executor.service';
 import { TaskController } from '../controllers/task.controller';
 import { TaskRepository } from '../repositories/task-repository';
+
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
-  providers: [TasksService, TaskExecutorService, TaskRepository], controllers: [TaskController],
-  })
+  providers: [TasksService, TaskRepository],
+  controllers: [TaskController],
+  exports: [TasksService]
+})
 export class TasksModule {}
