35d2cf5f85d3ca8040ba0598adb6fa9ac8b53d31
fukkingsnow <fukkingsnow@gmail.com>
2026-02-05 17:02:40 +0300

    Update common.module.ts

--- src/modules/common.module.ts
+++ src/modules/common.module.ts
@@ -3,23 +3,22 @@ import { CommonService } from '../services/common.service';
 import { MetricsService } from '../services/metrics.service';
 import { AutonomousStrategyAnalyzer } from '../services/autonomous-strategy.analyzer';
 import { FileSystemService } from '../services/file-system.service';
+import { GitHubService } from '../services/github.service'; // ДОБАВИТЬ ЭТОТ ИМПОРТ
 
-/**
- * Common Feature Module
- * Провайдеры для автономной работы ARQ
- */
 @Module({
   providers: [
     CommonService,
     MetricsService,
     AutonomousStrategyAnalyzer,
-    FileSystemService
+    FileSystemService,
+    GitHubService, // ДОБАВИТЬ СЮДА
   ],
   exports: [
     CommonService,
     MetricsService,
     AutonomousStrategyAnalyzer,
-    FileSystemService
+    FileSystemService,
+    GitHubService, // И СЮДА
   ],
 })
 export class CommonModule {}
