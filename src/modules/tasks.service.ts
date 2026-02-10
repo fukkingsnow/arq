0a695c58044a95d8e8f95d7f04db9370f7054482
fukkingsnow <fukkingsnow@gmail.com>
2026-02-10 18:59:51 +0300

    Update tasks.service.ts

--- src/modules/tasks.service.ts
+++ src/modules/tasks.service.ts
@@ -1,40 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from '../entities/task.entity';
+import { TasksController } from '../modules/tasks.controller'; // Правильный путь
+import { TasksService } from '../modules/tasks.service';     // Правильный путь
 
-@Injectable()
-export class TasksService {
-  constructor(
-    @InjectRepository(Task)
-    private taskRepository: Repository<Task>,
-  ) {}
-
-  async findAll(): Promise<Task[]> {
-    return await this.taskRepository.find({ order: { createdAt: 'DESC' } });
-  }
-
-  async findOne(id: string): Promise<Task> {
-    // Используем id as any, чтобы TypeORM не ругался на типы UUID/String
-    const task = await this.taskRepository.findOne({ where: { id: id as any } });
-    if (!task) throw new NotFoundException(`Task with ID ${id} not found`);
-    return task;
-  }
-
-  async create(data: Partial<Task>): Promise<Task> {
-    const task = this.taskRepository.create(data);
-    return await this.taskRepository.save(task);
-  }
-
-  // ТОТ САМЫЙ МЕТОД, ИЗ-ЗА КОТОРОГО ВСЁ ПАДАЛО
-  async update(id: string, data: Partial<Task>): Promise<Task> {
-    const task = await this.findOne(id);
-    Object.assign(task, data);
-    return await this.taskRepository.save(task);
-  }
-
-  async remove(id: string): Promise<void> {
-    const task = await this.findOne(id);
-    await this.taskRepository.remove(task);
-  }
-}
+@Module({
+  imports: [TypeOrmModule.forFeature([Task])],
+  controllers: [TasksController],
+  providers: [TasksService],
+  exports: [TasksService],
+})
+export class TaskModule {}
